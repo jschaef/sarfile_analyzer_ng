197d59f0db63df3f9279a6369ba27b3f2902e42b
Jochen Schäfer <ajoschz@gmail.com>
2023-11-22 15:30:04 +0100

    Function added to delete _obj keys
    This is a generic function which deletes all keys
    ending with _obj in session state.
    It will be used from other modules
    
    Furthermore the code syntax checker did a lot of
    cleanup in sense of exchanging ' by " and inserting
    whitespaces e.g.: a=b to a = b

--- code/layout_helper_pl.py
+++ code/layout_helper_pl.py
@@ -7,20 +7,21 @@ from altair_saver import save
 
 global fobject
 
+
 def create_pdf(file: str, chart: object) -> list:
-   if not st.session_state.get('download'):
-        mimetype = 'text/plain'
-        return ['dummy', mimetype]
-   else:
+    if not st.session_state.get("download"):
+        mimetype = "text/plain"
+        return ["dummy", mimetype]
+    else:
         global fobject
-        mimetype = 'application/x-binary'
+        mimetype = "application/x-binary"
         save(chart, file)
-        fobject = open(file, 'rb')
+        fobject = open(file, "rb")
         st.session_state.download = False
         return [fobject, mimetype]
 
 
-def pdf_download(file: str, chart: object,key=None, download_name=None):
+def pdf_download(file: str, chart: object, key=None, download_name=None):
     """creates a download button and a checkbox. Using function create_pdf()
     to create the file for download if the checkbox has been enabled.
 
@@ -29,25 +30,30 @@ def pdf_download(file: str, chart: object,key=None, download_name=None):
         chart (altair object): image/chart created from the altair library
         key (widget key, optional): widget key for streamlit api. Defaults to None.
     """
-    disabled=True
+    disabled = True
     st.session_state.download = False
-    col1, col2, *_ = st.columns([0.1,0.1,0.8])
+    col1, col2, *_ = st.columns([0.1, 0.1, 0.8])
     col2 = col2 if col2 else st
     save_dir = os.path.dirname(file)
     if not os.path.exists(save_dir):
-        os.system(f'mkdir -p {save_dir}')
+        os.system(f"mkdir -p {save_dir}")
     dkey = f"pdf_{random()}_d"
-    clicked = col2.checkbox('*Enable Download*', key=key)
+    clicked = col2.checkbox("*Enable Download*", key=key)
     if clicked:
         st.session_state.download = True
         disabled = False
 
     if not download_name:
-       download_name = 'sar_chart.pdf' 
+        download_name = "sar_chart.pdf"
 
-    col1.download_button(label='Download PDF', file_name=download_name, 
+    col1.download_button(
+        label="Download PDF",
+        file_name=download_name,
         data=create_pdf(file, chart)[0],
-        mime='application/x-binary',key=dkey, disabled=disabled)
+        mime="application/x-binary",
+        key=dkey,
+        disabled=disabled,
+    )
     try:
         fobject
     except NameError:
@@ -55,64 +61,67 @@ def pdf_download(file: str, chart: object,key=None, download_name=None):
     else:
         fobject.close()
 
+
 def show_metrics(prop_list, col=None, key=None, checkbox=None):
     col = col if col else st
     if key:
-        if col.checkbox('Show Metric descriptions from man page', key=key):
+        if col.checkbox("Show Metric descriptions from man page", key=key):
             for metric in prop_list:
                 helpers.metric_expander(metric, col=col)
     elif checkbox != "off":
-        if col.checkbox('Show Metric descriptions from man page'):
+        if col.checkbox("Show Metric descriptions from man page"):
             for metric in prop_list:
                 helpers.metric_expander(metric, col=col)
     else:
         for metric in prop_list:
             helpers.metric_expander(metric, col=col)
 
-def create_columns(number: int, write_field: list=None) -> list:
+
+def create_columns(number: int, write_field: list = None) -> list:
     """Create columns and write empty string into them
        if the column index in write_field is True
     Args:
         number (integer): number of columns
-        write_field (list): 
+        write_field (list):
     """
     cols = st.columns(number)
     if write_field:
         for entry in range(len(write_field)):
             if write_field[entry]:
                 col = cols[entry]
-                col.write('')
-    return(cols)
+                col.write("")
+    return cols
+
 
 def arrange_grid_entries(object_field, cols_per_line):
     # check if there are selectboxes < cols_per_line left
     total_columns = len(object_field)
-    even_lines = int(total_columns/cols_per_line) 
+    even_lines = int(total_columns / cols_per_line)
     remaining_cols = total_columns % cols_per_line
     rcols = remaining_cols
     empty_cols = cols_per_line - rcols
     if even_lines == 0:
         even_lines = 1
         remaining_cols = 0
-    st.markdown('___')
+    st.markdown("___")
     pcols = st.columns(cols_per_line)
 
     for _ in range(even_lines):
         collect_field = []
         # more than cols_per_line found
         if total_columns >= cols_per_line:
-            for index in range(cols_per_line): 
+            for index in range(cols_per_line):
                 if object_field:
                     object = object_field.pop(0)
                     collect_field.append(object)
-            for index in range(len(collect_field)): 
+            for index in range(len(collect_field)):
                 object = collect_field[index][0]
                 stats = collect_field[index][1]
                 header = collect_field[index][2]
                 if header:
-                    pcols[index].markdown(f'###### {header}')
+                    pcols[index].markdown(f"###### {header}")
                 pcols[index].write(object)
-                pcols[index].markdown('###### statistics')
+                pcols[index].markdown("###### statistics")
                 pcols[index].write(stats)
 
         # less than cols_per_line found
@@ -121,19 +130,18 @@ def arrange_grid_entries(object_field, cols_per_line):
                 if object_field:
                     object = object_field.pop(0)
                     collect_field.append(object)
-            for index in range(len(collect_field)): 
+            for index in range(len(collect_field)):
                 object = collect_field[index][0]
                 stats = collect_field[index][1]
                 header = collect_field[index][2]
                 if header:
-                    pcols[index].markdown(f'###### {header}')
-                    for nindex in range(1,empty_cols +1):
+                    pcols[index].markdown(f"###### {header}")
+                    for nindex in range(1, empty_cols + 1):
                         nindex = cols_per_line - nindex
-                        pcols[nindex].write('')
+                        pcols[nindex].write("")
                 pcols[index].write(object)
-                pcols[index].markdown('###### statistics')
+                pcols[index].markdown("###### statistics")
                 pcols[index].write(stats)
-
 
     # remaining tables
     if remaining_cols:
@@ -142,38 +150,39 @@ def arrange_grid_entries(object_field, cols_per_line):
             if object_field:
                 object = object_field.pop()
                 collect_field.append(object)
-        for index in range(len(collect_field)): 
+        for index in range(len(collect_field)):
             object = collect_field[index][0]
             stats = collect_field[index][1]
             header = collect_field[index][2]
             if header:
-                pcols[index].markdown('___')
-                pcols[index].markdown(f'###### {header}')
-                for nindex in range(1,rcols+1):
+                pcols[index].markdown("___")
+                pcols[index].markdown(f"###### {header}")
+                for nindex in range(1, rcols + 1):
                     nindex = cols_per_line - nindex
-                    pcols[nindex].write('')
+                    pcols[nindex].write("")
             pcols[index].write(object)
-            pcols[index].markdown('###### statistics')
+            pcols[index].markdown("###### statistics")
             pcols[index].write(stats)
+
 
 def display_averages(dia_field, prop, main_title, sub_item):
     final_dfs = []
     final_dfs_sum = []
-    col1, col2 = st.columns([0.3,0.7])
+    col1, col2 = st.columns([0.3, 0.7])
     if sub_item:
-        col1.markdown(f'##### Average statistics for {main_title}/{sub_item}/{prop}')
-        col2.markdown(f'##### Average statistics for {main_title}/{sub_item}')
+        col1.markdown(f"##### Average statistics for {main_title}/{sub_item}/{prop}")
+        col2.markdown(f"##### Average statistics for {main_title}/{sub_item}")
     else:
-        col1.markdown(f'##### Average statistics for {main_title}/{prop}')
-        col2.markdown(f'##### Average statistics for {main_title}')
+        col1.markdown(f"##### Average statistics for {main_title}/{prop}")
+        col2.markdown(f"##### Average statistics for {main_title}")
 
     st.write("\n")
-    
+
     for entry in range(len(dia_field)):
-        st.markdown(f'- {dia_field[entry][0]}')
+        st.markdown(f"- {dia_field[entry][0]}")
 
     st.write("\n")
-    
+
     for entry in range(len(dia_field)):
         df = dia_field[entry][1][prop]
         df_sum = dia_field[entry][1]
@@ -183,8 +192,13 @@ def display_averages(dia_field, prop, main_title, sub_item):
         final_dfs_sum.append(df_sum)
     final_df = pd.concat(final_dfs)
     final_dfs_sum = pd.concat(final_dfs_sum)
-    col1, col2 = st.columns([0.3,0.7])
-    final_df.set_index('date', inplace=True)
-    final_dfs_sum.set_index('date', inplace=True)
+    col1, col2 = st.columns([0.3, 0.7])
+    final_df.set_index("date", inplace=True)
+    final_dfs_sum.set_index("date", inplace=True)
     col1.write(final_df.describe())
     col2.write(final_dfs_sum.describe())
+
+def delete_large_obj():
+    for item in st.session_state:
+        if "_obj" in item:
+            st.session_state.pop(item)
