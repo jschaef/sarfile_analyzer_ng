94f556bb496a520f470fe1f126a39e3e48c7e91f
Jochen Schäfer <ajoschz@gmail.com>
2024-09-30 13:24:44 +0200

    introduced using config from environment variables
      - using the same names as in Config class but
        just in uppercase, e.g.:
        redis_host = os.getenv("REDIS_HOST", "localhost")

--- code/config.py
+++ code/config.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+import os
+
+
 class Config(object):
-    upload_dir = 'upload'
-    redis_host = 'localhost'
-    redis_port = 6379
-    rkey_pref = 'user'
-    pdf_name = 'sar_chart.pdf'
-    admin_email = 'admin@example-org.com'
-    max_metric_header = 8
-    cols_per_line = 4
-    max_header_count = 6
-    file_type = 'parquet'
+    upload_dir = os.getenv("UPLOAD_DIR", "upload")
+    redis_host = os.getenv("REDIS_HOST", "localhost")
+    redis_port = int(os.getenv("REDIS_PORT", 6379))
+    rkey_pref = os.getenv("RKEY_PREF", "user")
+    pdf_name = os.getenv("PDF_NAME", "sar_chart.pdf")
+    admin_email = os.getenv("ADMIN_EMAIL", "admin@example-org.com")
+    max_metric_header = int(os.getenv("MAX_METRIC_HEADER", 8))
+    cols_per_line = int(os.getenv("COLS_PER_LINE", 4))
+    max_header_count = int(os.getenv("MAX_HEADER_COUNT", 6))
+    file_type = os.getenv("FILE_TYPE", "parquet")
